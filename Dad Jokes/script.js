0f109616d3d0cb92033bfa0ed306bbe501467ddc
Runal <bandishtirunal@gmail.com>
2021-09-26 00:16:36 +0530

    add dad jokes

---
+++ Dad Jokes/script.js
@@ -0,0 +1,30 @@
+const jokeEl = document.getElementById('jokes');
+const jokeBtn = document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click', generateJoke);
+
+generateJoke();
+
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  };
+  const res = await fetch('https://icanhazdadjoke.com/', config);
+  const data = await res.json();
+  jokeEl.innerHTML = data.joke;
+}
+
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   };
+//   fetch('https://icanhazdadjoke.com/', config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke;
+//     });
+// }
